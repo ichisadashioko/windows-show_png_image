c8ebd7c6658ae9ced7d67d3a7a76133098e14a62
shioko <ichisadashioko@gmail.com>
2023-03-07 21:16:16 +0900

    print png image info

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <tchar.h>
 
+#include <png.h>
+
 #define VK_Q_KEY 0x51
 #define QUIT_APPLICATION_KEY_CODE VK_Q_KEY
 
@@ -170,8 +172,136 @@ int CALLBACK wWinMain(                //
     return (int)msg.wParam;
 }
 
+// TODO write a function to decode PNG bytes
+unsigned char PNG_MAGIC_BYTES[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+
+int decode_png_bytes(   //
+    unsigned char *bs,  //
+    unsigned int length //
+)
+{
+    unsigned int current_index = 0;
+    // magic bytes
+    if (length < 8)
+    {
+        printf("decode_png_bytes failed at %s:%d\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    for (int i = 0; i < 8; i++)
+    {
+        if (bs[current_index + i] != PNG_MAGIC_BYTES[i])
+        {
+            printf("Invalid PNG file\ndecode_png_bytes failed at %s:%d\n", __FILE__, __LINE__);
+            return -1;
+        }
+    }
+
+    current_index += 8;
+
+    return 0;
+}
+
+int load_png_file_and_convert_to_bitmap(
+    char *filepath)
+{
+    FILE *fp = fopen(filepath, "rb");
+    if (!fp)
+    {
+        printf("fopen failed at %s:%d\nUnable to open file %s\n", __FILE__, __LINE__, filepath);
+        return -1;
+    }
+
+    // use libpng to decode the PNG file
+    png_structp png_ptr = png_create_read_struct( //
+        PNG_LIBPNG_VER_STRING,                    //
+        NULL,                                     //
+        NULL,                                     //
+        NULL                                      //
+    );
+
+    if (!png_ptr)
+    {
+        printf("png_create_read_struct failed at %s:%d\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr)
+    {
+        printf("png_create_info_struct failed at %s:%d\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+        printf("Error during init_io at %s:%d\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    png_init_io(png_ptr, fp);
+    png_read_info(png_ptr, info_ptr);
+
+    int width = png_get_image_width(png_ptr, info_ptr);
+    int height = png_get_image_height(png_ptr, info_ptr);
+    png_byte color_type = png_get_color_type(png_ptr, info_ptr);
+    png_byte bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+    printf("width: %d\n", width);
+    printf("height: %d\n", height);
+    printf("color_type: %d\n", color_type);
+    printf("bit_depth: %d\n", bit_depth);
+
+    int number_of_passes = png_set_interlace_handling(png_ptr);
+    png_read_update_info(png_ptr, info_ptr);
+
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+        printf("Error during read_image at %s:%d\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    png_bytep *row_pointers = (png_bytep *)malloc(sizeof(png_bytep) * height);
+    for (int y = 0; y < height; y++)
+    {
+        row_pointers[y] = (png_byte *)malloc(png_get_rowbytes(png_ptr, info_ptr));
+    }
+
+    png_read_image(png_ptr, row_pointers);
+    fclose(fp);
+
+    // convert the PNG image data to a bitmap
+    // TODO
+    for (int i = 0; i < height; i++)
+    {
+        png_byte *row = row_pointers[i];
+        for (int j = 0; j < width; j++)
+        {
+            png_byte *pixel_value_pointer = &(row[j * 3]);
+            // printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n", j, i, pixel_value_pointer[0], pixel_value_pointer[1], pixel_value_pointer[2], pixel_value_pointer[3]);
+
+            printf("(%d, %d, %d) ", pixel_value_pointer[0], pixel_value_pointer[1], pixel_value_pointer[2]);
+            // TODO set pixel color
+        }
+
+        printf("\n");
+    }
+
+    // free memory
+    for (int i = 0; i < height; i++)
+    {
+        free(row_pointers[i]);
+    }
+    free(row_pointers);
+
+    return 0;
+}
+
 int main()
 {
+    char *image_filepath = "test_2x2_image.png";
+    load_png_file_and_convert_to_bitmap(image_filepath);
+
     return wWinMain(           //
         GetModuleHandle(NULL), //
         NULL,                  //
