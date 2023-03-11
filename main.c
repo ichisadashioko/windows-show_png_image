2a11073ae83a8b1e5b6b776f2409a639ec0ce063
shioko <ichisadashioko@gmail.com>
2023-03-11 15:06:14 +0900

    improve pixels data copying operation speed

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 
 #include <png.h>
 
+#include "termcolor.h"
+
 #define VK_Q_KEY 0x51
 #define QUIT_APPLICATION_KEY_CODE VK_Q_KEY
 
@@ -32,7 +34,44 @@ IS_8BIT_IMAGE GLOBAL_IS_IMAGE = {
     .type = 0,   //
     .data = NULL //
 };
+
+void *mMalloc(                  //
+    size_t request_memory_size, //
+    char *caller_filepath,      //
+    int caller_line_number      //
+)
+{
+    void *retval = malloc(request_memory_size);
+    if (retval == NULL)
+    {
+        printf(FG_RED);
+        printf("malloc failed to allocate %d byte(s) at %s:%d\n", request_memory_size, caller_filepath, caller_line_number);
+        printf(RESET_COLOR);
+        exit(-1);
+    }
+
+    return retval;
+}
 // BITMAP
+
+int IS_8BIT_IMAGE_TO_BITMAP( //
+    IS_8BIT_IMAGE is_image   //
+)
+{
+    // I want to create a bitmap from the IS_8BIT_IMAGE to cache the pixel-copying operation.
+    HDC HDC_screen = GetDC(NULL);
+    HBITMAP HBITMAP_cache_bitmap = CreateCompatibleBitmap( //
+        HDC_screen,                                        //
+        is_image.width,                                    //
+        is_image.height                                    //
+    );
+
+    SelectObject(HDC_screen, HBITMAP_cache_bitmap);
+    // TODO
+
+    return 0;
+}
+
 void mHandleResizeMessage( //
     _In_ HWND hwnd,        //
     _In_ UINT uMsg,        //
@@ -87,19 +126,30 @@ void mHandlePaintMessage( //
         HDC bitmap_hdc = CreateCompatibleDC(hdc);
         SelectObject(bitmap_hdc, bitmap_handle);
 
-        // modify the pixel values
-        for (unsigned int y = 0; y < GLOBAL_IS_IMAGE.height; y++)
-        {
-            for (unsigned int x = 0; x < GLOBAL_IS_IMAGE.width; x++)
-            {
-                unsigned int index = (y * GLOBAL_IS_IMAGE.width + x) * 3;
-                unsigned char r = GLOBAL_IS_IMAGE.data[index + 0];
-                unsigned char g = GLOBAL_IS_IMAGE.data[index + 1];
-                unsigned char b = GLOBAL_IS_IMAGE.data[index + 2];
+        // copy the image data into the bitmap
+        BITMAPINFO bitmap_info0 = {0};
+        bitmap_info0.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bitmap_info0.bmiHeader.biWidth = GLOBAL_IS_IMAGE.width;
+        bitmap_info0.bmiHeader.biHeight = GLOBAL_IS_IMAGE.height;
+        bitmap_info0.bmiHeader.biPlanes = 1;
+        bitmap_info0.bmiHeader.biBitCount = 24;
+        bitmap_info0.bmiHeader.biCompression = BI_RGB;
+        bitmap_info0.bmiHeader.biSizeImage = 0;
+        bitmap_info0.bmiHeader.biXPelsPerMeter = 0;
+        bitmap_info0.bmiHeader.biYPelsPerMeter = 0;
+        bitmap_info0.bmiHeader.biClrUsed = 0;
+        bitmap_info0.bmiHeader.biClrImportant = 0;
 
-                SetPixel(bitmap_hdc, x, y, RGB(r, g, b));
-            }
-        }
+        // modify the pixel values
+        int _retval = SetDIBits(    //
+            bitmap_hdc,             // [in] HDC hdc - A handle to a device context.
+            bitmap_handle,          // [in] HBITMAP hbm - A handle to the compatible bitmap (DDB) that is to be altered using the color data from the specified DIB.
+            0,                      // [in] UINT start - The starting scan line for the device-independent color data in the array pointed to by the lpvBits parameter.
+            GLOBAL_IS_IMAGE.height, // [in] UINT cLines - The number of scan lines found in the array containing device-independent color data.
+            GLOBAL_IS_IMAGE.data,   // [in] const VOID *lpBits - A pointer to the DIB color data, stored as an array of bytes. The format of the bitmap values depends on the biBitCount member of the BITMAPINFO structure pointed to by the lpbmi parameter.
+            &bitmap_info0,          // [in] const BITMAPINFO *lpbmi, - A pointer to a BITMAPINFO structure that contains information about the DIB.
+            DIB_RGB_COLORS          // [in] UINT colorUse - Indicates whether the bmiColors contains explicit red, green, blue (RGB) values or palette indexes. The fuColorUse parameter must be one of the following values. DIB_PAL_COLORS - The color table consists of an array of 16-bit indexes into the logical palette of the device context identified by the HDC parameter. DIB_RGB_COLORS - The color table is provided and contains literal RGB values.
+        );
 
         // draw the bitmap
         BitBlt(hdc, 0, 0, GLOBAL_IS_IMAGE.width, GLOBAL_IS_IMAGE.height, bitmap_hdc, 0, 0, SRCCOPY);
@@ -340,68 +390,43 @@ int load_png_file_and_convert_to_IS_IMAGE( //
             return -1;
         }
 
-        png_bytep *row_pointers = (png_bytep *)malloc(sizeof(png_bytep) * height);
-        if (!row_pointers)
+        size_t png_bytep_array_size = (size_t)(sizeof(png_bytep) * height);
+        png_bytep *row_pointers = (png_bytep *)mMalloc(png_bytep_array_size, __FILE__, __LINE__);
+        for (int y = 0; y < height; y++)
         {
-            printf("malloc failed at %s:%d\n", __FILE__, __LINE__);
-            retval = -1;
+            row_pointers[y] = (png_byte *)malloc(png_get_rowbytes(png_ptr, info_ptr));
         }
-        else
+
+        png_read_image(png_ptr, row_pointers);
+
+        // IS_8BIT_IMAGE is_image0 = {0};
+
+        is_image_pointer->width = (unsigned int)width;
+        is_image_pointer->height = (unsigned int)height;
+        is_image_pointer->type = IS_IMAGE_TYPE_RGB;
+        size_t data_length = (size_t)(width * height * 3);
+        is_image_pointer->data = (unsigned char *)mMalloc(data_length, __FILE__, __LINE__);
+        for (int i = 0; i < height; i++)
         {
-            for (int y = 0; y < height; y++)
+            png_byte *row = row_pointers[i];
+            for (int j = 0; j < width; j++)
             {
-                row_pointers[y] = (png_byte *)malloc(png_get_rowbytes(png_ptr, info_ptr));
+                png_byte *pixel_value_pointer = &(row[j * 3]);
+                // printf("(%d, %d, %d) ", pixel_value_pointer[0], pixel_value_pointer[1], pixel_value_pointer[2]);
+                // TODO set pixel color
+                int is_image_data_index = (i * width * 3) + (j * 3);
+                is_image_pointer->data[is_image_data_index + 0] = pixel_value_pointer[0];
+                is_image_pointer->data[is_image_data_index + 1] = pixel_value_pointer[1];
+                is_image_pointer->data[is_image_data_index + 2] = pixel_value_pointer[2];
             }
-
-            png_read_image(png_ptr, row_pointers);
-
-            // IS_8BIT_IMAGE is_image0 = {0};
-
-            is_image_pointer->width = (unsigned int)width;
-            is_image_pointer->height = (unsigned int)height;
-            is_image_pointer->type = IS_IMAGE_TYPE_RGB;
-            unsigned int data_length = (unsigned int)(width * height * 3);
-            is_image_pointer->data = (unsigned char *)malloc(data_length);
-            if (!is_image_pointer->data)
-            {
-                printf("malloc failed at %s:%d\n", __FILE__, __LINE__);
-                retval = -1;
-            }
-            else
-            {
-                // convert the PNG image data to a bitmap
-                // HBITMAP hbitmap0 = CreateBitmap( //
-                //     width,                       // [in] int nWidth
-                //     height,                      // [in] int nHeight
-                //     1,                           // [in] UINT cPlanes
-                //     24,                           // [in] UINT cBitsPerPel
-                // );
-                // TODO
-                for (int i = 0; i < height; i++)
-                {
-                    png_byte *row = row_pointers[i];
-                    for (int j = 0; j < width; j++)
-                    {
-                        png_byte *pixel_value_pointer = &(row[j * 3]);
-                        // printf("(%d, %d, %d) ", pixel_value_pointer[0], pixel_value_pointer[1], pixel_value_pointer[2]);
-                        // TODO set pixel color
-                        int is_image_data_index = (i * width * 3) + (j * 3);
-                        is_image_pointer->data[is_image_data_index + 0] = pixel_value_pointer[0];
-                        is_image_pointer->data[is_image_data_index + 1] = pixel_value_pointer[1];
-                        is_image_pointer->data[is_image_data_index + 2] = pixel_value_pointer[2];
-                    }
-
-                    // printf("\n");
-                }
-            }
-
-            // free memory
-            for (int i = 0; i < height; i++)
-            {
-                free(row_pointers[i]);
-            }
-            free(row_pointers);
         }
+
+        // free memory
+        for (int i = 0; i < height; i++)
+        {
+            free(row_pointers[i]);
+        }
+        free(row_pointers);
     }
 
     fclose(fp);
